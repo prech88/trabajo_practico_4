52c1590a08df75bc46398ebe1088813e9102ca35
Matias <Matias@DESKTOP-5683VC2>
2024-05-28 19:16:27 -0300

    Se crean los modelos Materia, Docente, Alumno y Carrera.

---
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,95 @@
+package ar.edu.unju.fi.model;
+
+import java.time.LocalDate;
+
+public class Alumno {
+	private Integer dni;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private Long telefono;
+	private LocalDate fechaNac;
+	private String domicilio;
+	private Integer lu;
+	
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Alumno(Integer dni, String nombre, String apellido, String email, Long telefono, LocalDate fechaNac,
+			String domicilio, Integer lu) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+		this.fechaNac = fechaNac;
+		this.domicilio = domicilio;
+		this.lu = lu;
+	}
+
+	public Integer getDni() {
+		return dni;
+	}
+
+	public void setDni(Integer dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(Long telefono) {
+		this.telefono = telefono;
+	}
+
+	public LocalDate getFechaNac() {
+		return fechaNac;
+	}
+
+	public void setFechaNac(LocalDate fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	public Integer getLu() {
+		return lu;
+	}
+
+	public void setLu(Integer lu) {
+		this.lu = lu;
+	}
+	
+}
---
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -0,0 +1,52 @@
+package ar.edu.unju.fi.model;
+
+public class Carrera {
+	private Integer codigo;
+	private String nombre;
+	private Byte cantAños;
+	private Boolean estado;
+	
+	public Carrera() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Carrera(Integer codigo, String nombre, Byte cantAños, Boolean estado) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.cantAños = cantAños;
+		this.estado = estado;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Byte getCantAños() {
+		return cantAños;
+	}
+
+	public void setCantAños(Byte cantAños) {
+		this.cantAños = cantAños;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+	
+}
