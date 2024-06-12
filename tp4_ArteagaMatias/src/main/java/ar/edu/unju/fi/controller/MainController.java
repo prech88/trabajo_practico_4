63ea28a78492a8a56891e9b763ecd8f3b02541db
Matias <Matias@DESKTOP-5683VC2>
2024-06-12 04:43:50 -0300

    Se terminan las partes de header y footer en los layouts para poder impementarlos en las paginas.

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,13 +1,17 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class MainController {
 	
 	@GetMapping("/principal")
-	public String getIndexPage() {
+	public String getIndexPage(Model model) {
+		model.addAttribute("titulo", "Principal");
 		return "index";
 	}
 }
