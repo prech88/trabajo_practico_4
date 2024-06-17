ec471f60d1850773779eae9703e05c4265b06458
Matias <Matias@DESKTOP-5683VC2>
2024-06-17 19:58:38 -0300

    Se agrega la dependencia lombok y se cambia el modelo de la clase Carrera

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -2,54 +2,21 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Carrera {
 	private Integer codigo;
 	private String nombre;
 	private Byte cantAños;
-	private Boolean estado;
-	
-	public Carrera() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Carrera(Integer codigo, String nombre, Byte cantAños, Boolean estado) {
-		this.codigo = codigo;
-		this.nombre = nombre;
-		this.cantAños = cantAños;
-		this.estado = estado;
-	}
-
-	public Integer getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(Integer codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public Byte getCantAños() {
-		return cantAños;
-	}
-
-	public void setCantAños(Byte cantAños) {
-		this.cantAños = cantAños;
-	}
-
-	public Boolean getEstado() {
-		return estado;
-	}
-
-	public void setEstado(Boolean estado) {
-		this.estado = estado;
-	}
-	
+	private Boolean estado;	
 }
