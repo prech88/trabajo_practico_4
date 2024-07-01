33db515ebe98d209b8a7d2b9be7797cd07fceb92
Leocampos <camposleonardo2504@gmail.com>
2024-07-01 00:19:04 -0300

    Adjunto Interfaz DocenteService

---
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/service/IDocenteService.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.service;
+
+import java.util.List;
+
+import ar.edu.unju.fi.dto.DocenteDto;
+
+public interface IDocenteService {
+List<DocenteDto> findAll();
+	
+	DocenteDto findById(Integer codigo);
+	
+	void saveDocenteDto (DocenteDto docenteDto);
+	
+	void deleteByID(Integer codigo);
+	
+	void edit(DocenteDto DocenteDto);
+}
