5ee871239b253b589d3b06e8f956e67ead17acbf
Leocampos <camposleonardo2504@gmail.com>
2024-07-01 00:11:13 -0300

    Adjunto el model Docente con la implemetacion de lombok

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -2,54 +2,22 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Docente {
 	private Integer legajo;
 	private String nombre;
 	private String apellido;
 	private Long telefono;
-	
-	public Docente() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Docente(Integer legajo, String nombre, String apellido, Long telefono) {
-		this.legajo = legajo;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.telefono = telefono;
-	}
-
-	public Integer getLegajo() {
-		return legajo;
-	}
-
-	public void setLegajo(Integer legajo) {
-		this.legajo = legajo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-	public Long getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(Long telefono) {
-		this.telefono = telefono;
-	}
 	
 }
