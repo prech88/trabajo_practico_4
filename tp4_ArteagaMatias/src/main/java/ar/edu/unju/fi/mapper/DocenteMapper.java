d5e1b5c316692474366d9efef086c17e01a2fe03
Leocampos <camposleonardo2504@gmail.com>
2024-07-01 00:18:06 -0300

    Adjunto DocenteMapper

---
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/mapper/DocenteMapper.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.mapper;
+
+import java.util.List;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.springframework.stereotype.Component;
+
+import ar.edu.unju.fi.dto.DocenteDto;
+import ar.edu.unju.fi.model.Docente;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Component
+
+public interface DocenteMapper {
+DocenteDto toDocenteDto(Docente docente);
+	
+	@InheritInverseConfiguration
+	Docente toDocente(DocenteDto docenteDto);
+	
+	List<DocenteDto> toDocenteDtoList(List<Docente> docente);
+	
+	List<Docente> toDocenteList(List<DocenteDto> docentesDto);
+}
