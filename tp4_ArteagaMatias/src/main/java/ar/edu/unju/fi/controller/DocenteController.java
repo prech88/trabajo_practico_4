7886abe234d6849a04c0852c4b428fa829ed2fb1
Leocampos <camposleonardo2504@gmail.com>
2024-07-01 00:22:36 -0300

    Adjuntos modifificaciones en DocenteController implementando docenteService y docenteDto

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
@@ -10,18 +10,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import ar.edu.unju.fi.collections.CollectionDocente;
-import ar.edu.unju.fi.model.Docente;
+import ar.edu.unju.fi.dto.DocenteDto;
+import ar.edu.unju.fi.service.IDocenteService;
 
 @Controller
 @RequestMapping("/docente")
 public class DocenteController {
+
 	@Autowired
-	private Docente docente;
+	private DocenteDto docenteDto;
+	@Autowired
+	private IDocenteService docenteService;
 	
 	@GetMapping("/listado")
 	public String getDocentePage(Model model) {
-		model.addAttribute("docentes", CollectionDocente.getDocentes());
+		model.addAttribute("docentes", docenteService.findAll());
 		model.addAttribute("titulo", "Docentes");
 		return "docentes";
 	}
@@ -30,24 +33,24 @@ public class DocenteController {
 	public String getDocenteAltaPage(Model model) {
 		boolean edicion = false;
 		model.addAttribute("titulo", "Nuevo Docente");
-		model.addAttribute("docente", docente);
+		model.addAttribute("docente", docenteDto);
 		model.addAttribute("edicion", edicion);
 		return "docentesForm";
 	}
 	
 	@PostMapping("/guardar")
-	public ModelAndView guardarNuevoDocente(@ModelAttribute("docente") Docente docente) {
+	public ModelAndView guardarNuevoDocente(@ModelAttribute("docente") DocenteDto docenteDto) {
 		ModelAndView modelView = new ModelAndView("docentes");
-		CollectionDocente.agregarDocente(docente);
-		modelView.addObject("docentes", CollectionDocente.getDocentes());
+		docenteService.saveDocenteDto(docenteDto);
+		modelView.addObject("docentes", docenteService.findAll());
 		return modelView;
 	}
 	
 	@GetMapping("/modificar/{legajo}")
 	public String getDocentesModificarPage(Model model, @PathVariable(value="legajo")int legajo) {
-		Docente docenteEncontrado = new Docente();
+		DocenteDto docenteEncontrado = new DocenteDto();
 		boolean edicion = true;
-		docenteEncontrado = CollectionDocente.buscarDocente(legajo);
+		docenteEncontrado = docenteService.findById(legajo);
 		model.addAttribute("edicion", edicion);
 		model.addAttribute("docente", docenteEncontrado);
 		model.addAttribute("titulo", "Modificar Docente");
@@ -55,14 +58,14 @@ public class DocenteController {
 	}
 	
 	@PostMapping("/modificar")
-	public String modificarDocente(@ModelAttribute("docente") Docente docente) {
-		CollectionDocente.modificarDocente(docente);
+	public String modificarDocente(@ModelAttribute("docente") DocenteDto docenteDto) {
+		docenteService.edit(docenteDto);
 		return "redirect:/docente/listado";
 	}
 	
 	@GetMapping("/eliminar/{legajo}")
 	public String eliminarDocente(@PathVariable(value="legajo")int legajo) {
-		CollectionDocente.elminarDocente(legajo);
+		docenteService.deleteByID(legajo);
 		return "redirect:/docente/listado";
 	}
 	
