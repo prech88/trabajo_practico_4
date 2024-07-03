7ab8d94029dedd7d5340307f2f50343ce2388431
Matias <Matias@DESKTOP-5683VC2>
2024-07-03 16:47:43 -0300

    Se agregan las notaciones de lombook a la clase Alumno.

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Alumno.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -4,8 +4,25 @@ import java.time.LocalDate;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "carreras")
 public class Alumno {
+	@Id
 	private Integer dni;
 	private String nombre;
 	private String apellido;
@@ -14,85 +31,4 @@ public class Alumno {
 	private LocalDate fechaNac;
 	private String domicilio;
 	private Integer lu;
-	
-	public Alumno() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Alumno(Integer dni, String nombre, String apellido, String email, Long telefono, LocalDate fechaNac,
-			String domicilio, Integer lu) {
-		this.dni = dni;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.email = email;
-		this.telefono = telefono;
-		this.fechaNac = fechaNac;
-		this.domicilio = domicilio;
-		this.lu = lu;
-	}
-
-	public Integer getDni() {
-		return dni;
-	}
-
-	public void setDni(Integer dni) {
-		this.dni = dni;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Long getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(Long telefono) {
-		this.telefono = telefono;
-	}
-
-	public LocalDate getFechaNac() {
-		return fechaNac;
-	}
-
-	public void setFechaNac(LocalDate fechaNac) {
-		this.fechaNac = fechaNac;
-	}
-
-	public String getDomicilio() {
-		return domicilio;
-	}
-
-	public void setDomicilio(String domicilio) {
-		this.domicilio = domicilio;
-	}
-
-	public Integer getLu() {
-		return lu;
-	}
-
-	public void setLu(Integer lu) {
-		this.lu = lu;
-	}
-	
 }
