e210af308bead2e65d27edf89652f7fb4a447b72
LeoCampos2504 <165981952+LeoCampos2504@users.noreply.github.com>
2024-07-03 23:13:12 -0300

    Update Docente.java
    se agrega @id @Entity @Table

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -2,6 +2,9 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +17,10 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "docentes")
 public class Docente {
+	@Id
 	private Integer legajo;
 	private String nombre;
 	private String apellido;
