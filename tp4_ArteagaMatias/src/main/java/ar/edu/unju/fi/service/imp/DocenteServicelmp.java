64add43a6b8d0305adba3f9dac9b42d1fd1c20ca
LeoCampos2504 <165981952+LeoCampos2504@users.noreply.github.com>
2024-07-03 23:16:21 -0300

    Update DocenteServicelmp.java
    Se modifica para usar DocenteRepository

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/service/imp/DocenteServicelmp.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/service/imp/DocenteServicelmp.java
@@ -1,44 +1,52 @@
 package ar.edu.unju.fi.service.imp;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ar.edu.unju.fi.collections.CollectionDocente;
+
 import ar.edu.unju.fi.dto.DocenteDto;
 import ar.edu.unju.fi.mapper.DocenteMapper;
+import ar.edu.unju.fi.model.Docente;
+import ar.edu.unju.fi.repository.DocenteRepository;
 import ar.edu.unju.fi.service.IDocenteService;
 @Service
 public class DocenteServicelmp implements IDocenteService {
 	@Autowired
 	private DocenteMapper docenteMapper;
-
+	@Autowired
+	private DocenteRepository docenteRepository;
 	@Override
 	public List<DocenteDto> findAll() {
-		List<DocenteDto> docenteDto = docenteMapper.toDocenteDtoList(CollectionDocente.getDocentes());
-		return docenteDto;
+		List<Docente> docentes = docenteRepository.findAll();
+		List<DocenteDto> docentesDto = docenteMapper.toDocenteDtoList(docentes);
+		return docentesDto;
 	}
 
 	@Override
 	public DocenteDto findById(Integer codigo) {
-		DocenteDto docenteDto = docenteMapper.toDocenteDto(CollectionDocente.buscarDocente(codigo));
-		return docenteDto;
+		Optional<Docente> docenteBuscado = docenteRepository.findById(codigo);
+		return docenteBuscado.map(docenteMapper::toDocenteDto).orElse(null);
 	}
 
 	@Override
 	public void saveDocenteDto (DocenteDto docenteDto) {
-		CollectionDocente.agregarDocente(docenteMapper.toDocente(docenteDto));
+		Docente docente = docenteMapper.toDocente(docenteDto);
+		docenteRepository.save(docente);
 	}
 
 	@Override
 	public void deleteByID(Integer codigo) {
-		CollectionDocente.elminarDocente(codigo);
+		docenteRepository.deleteById(codigo);
 	}
 
 	@Override
 	public void edit(DocenteDto docenteDto) {
-		CollectionDocente.modificarDocente(docenteMapper.toDocente(docenteDto));
+		Docente docente = docenteMapper.toDocente(docenteDto);
+		docenteRepository.save(docente);
 	}
 
 }
+
