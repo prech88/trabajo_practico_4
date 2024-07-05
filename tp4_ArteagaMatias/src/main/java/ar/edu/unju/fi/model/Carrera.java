d3e8bae87782a25da7a766cd60a66eb7f913de23
Matias <Matias@DESKTOP-5683VC2>
2024-07-04 22:45:20 -0300

    Se mapea Materia y Carrera. Tambien se corrije CollectionCarrera que presentaba un error al llenar los datos ya que se agrego una lista de materias en la clase Carrera.

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,9 +1,13 @@
 package ar.edu.unju.fi.model;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,5 +28,7 @@ public class Carrera {
 	private Integer codigo;
 	private String nombre;
 	private Byte cantAnios;
-	private Boolean estado;	
+	private Boolean estado;
+	@OneToMany(mappedBy = "carrera", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Materia> materias;
 }
--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Materia.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -1,89 +1,38 @@
 package ar.edu.unju.fi.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "materias")
 public class Materia {
+	@Id
 	private Integer codigo;
 	private String nombre;
 	private String curso;
 	private Byte cantHoras;
 	private Boolean modalidad;
-	@Autowired
+	@OneToOne
+	@JoinColumn(name = "legajo")
 	private Docente docente;
-	@Autowired
+	@ManyToOne
+	@JoinColumn(name = "codigo")
 	private Carrera carrera;
-	
-	public Materia() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Materia(Integer codigo, String nombre, String curso, Byte cantHoras, Boolean modalidad, Docente docente,
-			Carrera carrera) {
-		this.codigo = codigo;
-		this.nombre = nombre;
-		this.curso = curso;
-		this.cantHoras = cantHoras;
-		this.modalidad = modalidad;
-		this.docente = docente;
-		this.carrera = carrera;
-	}
-
-	public Integer getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(Integer codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getCurso() {
-		return curso;
-	}
-
-	public void setCurso(String curso) {
-		this.curso = curso;
-	}
-
-	public Byte getCantHoras() {
-		return cantHoras;
-	}
-
-	public void setCantHoras(Byte cantHoras) {
-		this.cantHoras = cantHoras;
-	}
-
-	public Boolean getModalidad() {
-		return modalidad;
-	}
-
-	public void setModalidad(Boolean modalidad) {
-		this.modalidad = modalidad;
-	}
-
-	public Docente getDocente() {
-		return docente;
-	}
-
-	public void setDocente(Docente docente) {
-		this.docente = docente;
-	}
-
-	public Carrera getCarrera() {
-		return carrera;
-	}
-
-	public void setCarrera(Carrera carrera) {
-		this.carrera = carrera;
-	}
-	
 }
