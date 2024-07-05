40b53ad86a745ad19979dfa748aee34f6fbd945a
Matias <Matias@DESKTOP-5683VC2>
2024-07-04 23:22:40 -0300

    Se crea MateriaMapper y se mapea MateriaDTO y MAteria.

---
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/mapper/MateriaMapper.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.mapper;
+
+import java.util.List;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.springframework.stereotype.Component;
+
+import ar.edu.unju.fi.dto.MateriaDTO;
+import ar.edu.unju.fi.model.Materia;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = {CarreraMapper.class, DocenteMapper.class})
+@Component
+public interface MateriaMapper {
+	
+	@Mapping(target = "carrera", source = "carrera")
+    @Mapping(target = "docente", source = "docente")
+	MateriaDTO toMateriaDTO(Materia materia);
+	
+	@InheritInverseConfiguration
+	Materia materia(MateriaDTO materiaDTO);
+	
+	List<MateriaDTO> toMateriasDTO(List<Materia> materias);
+	
+	List<Materia> toMaterias(List<MateriaDTO> materiasDTO);
+	
+}
