37a70e712e5aae81591d3e4b3163cf665d47179f
Matias <Matias@DESKTOP-5683VC2>
2024-07-06 20:50:34 -0300

    Se crea un import.sql que carga un par de carreras y tres docentes a la bd.

---
+++ tp4_ArteagaMatias/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO carreras (codigo, nombre, cant_anios, estado) VALUES (1000, 'Ingeniería Informática', 5, true);
+INSERT INTO carreras (codigo, nombre, cant_anios, estado) VALUES (1001, 'Analista Programador Universitario', 3, true);
+INSERT INTO docentes (legajo, telefono, apellido, nombre) VALUES (2000, 432789, 'Vega', 'Ariel');
+INSERT INTO docentes (legajo, telefono, apellido, nombre) VALUES (2001, 458621, 'Carolina', 'Apaza');
+INSERT INTO docentes (legajo, telefono, apellido, nombre) VALUES (2003, 489543, 'Juan Carlos', 'Rodriguez');
