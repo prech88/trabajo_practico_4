3a1483eb4a5b2ed01f62fde3ab6b3585868d4851
Matias <Matias@DESKTOP-5683VC2>
2024-07-07 02:33:02 -0300

    Modificaciones para validar el legajo en Docente.

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Component;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -24,6 +27,9 @@ import lombok.ToString;
 @Table(name = "docentes")
 public class Docente {
 	@Id
+	@NotNull(message="Debe ingresar el legajo")
+    @Min(value = 1000, message = "El numero de legajo debe ser mayor a 1000")
+    @Max(value = 99999999, message = "El numero de legajo debe ser menor a 99999999")
 	private Integer legajo;
 	@NotBlank(message="Debe ingresar nombre para el docente")
     @Size(min=3, max=30, message="El nombre debe contener como mínimo 3 caracteres y como máximo 30 caracteres")
--- tp4_ArteagaMatias/src/main/resources/import.sql
+++ tp4_ArteagaMatias/src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO carreras (codigo, nombre, cant_anios, estado) VALUES (1000, 'Ingeniería Informática', 5, true);
 INSERT INTO carreras (codigo, nombre, cant_anios, estado) VALUES (1001, 'Analista Programador Universitario', 3, true);
-INSERT INTO docentes (legajo, telefono, apellido, nombre) VALUES (2000, 432789, 'Vega', 'Ariel');
-INSERT INTO docentes (legajo, telefono, apellido, nombre) VALUES (2001, 458621, 'Carolina', 'Apaza');
-INSERT INTO docentes (legajo, telefono, apellido, nombre) VALUES (2003, 489543, 'Juan Carlos', 'Rodriguez');
+INSERT INTO docentes (legajo, telefono, apellido, nombre) VALUES (1000, 432789, 'Vega', 'Ariel');
+INSERT INTO docentes (legajo, telefono, apellido, nombre) VALUES (1001, 458621, 'Carolina', 'Apaza');
+INSERT INTO docentes (legajo, telefono, apellido, nombre) VALUES (1003, 489543, 'Juan Carlos', 'Rodriguez');
