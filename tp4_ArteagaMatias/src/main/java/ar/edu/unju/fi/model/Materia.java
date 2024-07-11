c9b7ad5e79a4f0a4edab026d88b4d5f387069a29
Leocampos <camposleonardo2504@gmail.com>
2024-07-11 18:22:16 -0300

    subo mapeo Materia Alumno Inscripcion y agrego boton para filtrar en Materia

--- tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Materia.java
+++ tp4_ArteagaMatias/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -1,13 +1,17 @@
 package ar.edu.unju.fi.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.stereotype.Component;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -30,22 +34,30 @@ import lombok.ToString;
 @Entity
 @Table(name = "materias")
 public class Materia {
-	@Id
-	@NotNull(message="Debe ingresar el codigo")
+
+    @Id
+    @NotNull(message = "Debe ingresar el codigo")
     @Min(value = 1000, message = "El numero de codigo debe ser mayor a 1000")
     @Max(value = 99999999, message = "El numero de codigo debe ser menor a 99999999")
-	private Integer codigoMateria;
-	@NotBlank(message="Debe ingresar el nombre de la materia")
-    @Size(min=3, max=50, message="El nombre debe contener como mínimo 3 caracteres y como máximo 50 caracteres")
-    @Pattern(regexp= "[a-z A-Z ]*", message="Debe ingresar únicamente letras")
-	private String nombre;
-	private String curso;
-	private Byte cantHoras;
-	private Boolean modalidad;
-	@OneToOne
-	@JoinColumn(name = "legajo")
-	private Docente docente;
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "codigo")
-	private Carrera carrera;
+    private Integer codigoMateria;
+
+    @NotBlank(message = "Debe ingresar el nombre de la materia")
+    @Size(min = 3, max = 50, message = "El nombre debe contener como mínimo 3 caracteres y como máximo 50 caracteres")
+    @Pattern(regexp = "[a-z A-Z ]*", message = "Debe ingresar únicamente letras")
+    private String nombre;
+
+    private String curso;
+    private Byte cantHoras;
+    private Boolean modalidad;
+
+    @OneToOne
+    @JoinColumn(name = "legajo")
+    private Docente docente;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo")
+    private Carrera carrera;
+
+    @ManyToMany(mappedBy = "materias", fetch = FetchType.LAZY)
+    private Set<Alumno> alumnos = new HashSet<>();
 }
